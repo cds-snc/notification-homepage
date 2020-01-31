4bbe0215d9da04edc8721989e47a134e994074c1
Tim Arney <tim@line37.com>
2020-01-31 10:28:23 -0500

    section-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,40 @@ module.exports = {
       center: true,
       padding: "2rem"
     },
+    fontSize: {
+      xs: ".75rem",
+      sm: ".9rem",
+      base: "1.0rem",
+      "md": "1.2rem",
+      lg: "1.8rem",
+      xl: "2.0rem",
+      "brand": "2.0rem",
+      "2xl": "2.5rem",
+      "3xl": "2.875rem",
+      "4xl": "3.25rem"
+    },
+    fontFamily: {
+      display: ["lato"],
+      body: ["lato"]
+    },
     screens: {
       sm: "640px",
       md: "768px",
-      lg: "1024px",
+      lg: "1024px"
     },
     colors: {
+      white:{
+        default: "#FFF"
+      },
       blue: {
         lighter: "#B2E3FF",
         default: "#26374A"
       },
       gray: {
-        default: "#EEEEEE"
+        default: "#EEE"
       }
     },
     extend: {}
   },
-  variants: {},
   plugins: []
 };

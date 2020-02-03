92c4b825f8d39d7e4c6c9090582f4007e47e910d
Tim Arney <tim@line37.com>
2020-02-03 11:28:08 -0500

    mobile nav

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-import "./style.css"
+import "./style.css";
+
+function classToggle() {
+  const nav = document.querySelector(".mobile-nav");
+  const arrow = document.querySelector(".mobile-arrow");
+  nav.classList.toggle("hidden");
+  arrow.classList.toggle("flip");
+}
+document.querySelector("#menu").addEventListener("click", classToggle);
